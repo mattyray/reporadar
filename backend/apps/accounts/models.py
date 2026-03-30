40db2d2f3d65c268c705e933adfaa9ac57bcd5e7
mattyray <mnraynor90@gmail.com>
2026-03-30 17:01:02 -0400

    greenlight bruvolski

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -32,6 +32,7 @@ class APICredential(models.Model):
 
 
 
+
 def get_github_token(user):
     """Get GitHub OAuth token from allauth's SocialToken."""
     from allauth.socialaccount.models import SocialToken
